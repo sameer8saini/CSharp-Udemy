cac9666b2d52580bb5d437c44d714a5ebe63b4b4
sameer8saini <sameer8saini@gmail.com>
2024-01-30 22:32:51 +1300

    Add project files.

---
+++ FirstCSharpProject/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("How old are you?");
+
+string age = Console.ReadLine();
+
+// Console.WriteLine("You are " + age + " years old");
+
+//Console.WriteLine("You are {0} years old", age);
+
+Console.WriteLine($"You are {age} years old");
+
+// You are 37 years old
