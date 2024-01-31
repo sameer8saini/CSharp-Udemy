2b116abbb148abdb7f6242af137735234bdc63a7
sameer8saini <sameer8saini@gmail.com>
2024-01-31 15:11:07 +1300

    Keywords and Identifiers

--- FirstCSharpProject/Program.cs
+++ FirstCSharpProject/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.WriteLine("How old are you?");
 
-string age = Console.ReadLine();
+string @string = Console.ReadLine();
 
 // Console.WriteLine("You are " + age + " years old");
 
 //Console.WriteLine("You are {0} years old", age);
 
-Console.WriteLine($"You are {age} years old");
+Console.WriteLine($"You are {@string} years old");
 
 // You are 37 years old
---
+++ KeywordsAndIdentifiers/Program.cs
@@ -0,0 +1,18 @@
+﻿string firstName;
+string lastName;
+
+
+int age;
+int width;
+
+bool isAdult;
+
+
+// string string;
+// int int;
+
+// Use a reserved identifier using @
+string @string;
+@string = "Sameer";
+
+Console.WriteLine(@string);
