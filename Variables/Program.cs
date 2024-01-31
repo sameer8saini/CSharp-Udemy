e7e285e7af68109cdc2f8abf13d934ece99cecce
sameer8saini <sameer8saini@gmail.com>
2024-01-31 15:48:58 +1300

    Variables, Declaration, Assignments

---
+++ Variables/Program.cs
@@ -0,0 +1,14 @@
+﻿//string name;
+//name = "Sameer Saini";
+string name = "Sameer Saini";
+
+//int age;
+//age = 1;
+int age = 1;
+Console.WriteLine(age);
+
+age = 27;
+Console.WriteLine(age);
+
+int Age = 31;
+Console.WriteLine(Age);
