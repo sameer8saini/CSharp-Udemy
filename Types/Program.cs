77f36b450a834e2eb62e3774f6e7aba8a6086616
sameer8saini <sameer8saini@gmail.com>
2024-02-01 09:35:21 +1300

    Value Types

---
+++ Types/Program.cs
@@ -0,0 +1,32 @@
+﻿#region Boolean Type
+//bool isFormComplete = true;
+//bool isFormComplete = false;
+
+
+//bool isFormComplete = default;
+
+//The below will give a compiler error as 
+//a declared boolean variable cannot be assigned a string value
+//isFormComplete = "123";
+
+//Console.WriteLine(isFormComplete);
+#endregion
+
+#region Integral Types
+
+int minPopulation = int.MinValue;
+int maxPopulation = int.MaxValue;
+
+Console.WriteLine($"Min value is: {minPopulation}");
+Console.WriteLine($"Max value is: {maxPopulation}");
+
+#endregion
+
+#region Floating Types
+float float1 = 1234.123456F;
+float float2 = 1234.123456f;
+double double1 = 1234.1234567D;
+double double2 = 1234.1234567d;
+decimal decimal1 = 123456.123456978M;
+decimal decimal2 = 123456.123456978m;
+#endregion
