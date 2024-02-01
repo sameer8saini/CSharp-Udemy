c7fab048b47e6a561361a66234caf8cf1fdb4ce1
sameer8saini <sameer8saini@gmail.com>
2024-02-01 14:21:08 +1300

    Value Types vs Reference Types

--- Types/Program.cs
+++ Types/Program.cs
@@ -32,30 +32,46 @@
 #endregion
 
 #region String Types
-// Sameer 
-string name = "Sameer";
+//// Sameer 
+//string name = "Sameer";
 
-// Using escape character 
-// Output "Sameer"
-string nameWithQuotes = "\"Sameer\"";
+//// Using escape character 
+//// Output "Sameer"
+//string nameWithQuotes = "\"Sameer\"";
 
 
-// Print a path
-// C:\qfolder\qfile.txt
+//// Print a path
+//// C:\qfolder\qfile.txt
 
-string path = "C:\\qfolder\\qfile.txt";
+//string path = "C:\\qfolder\\qfile.txt";
 
-// Verbatim string literals
-string path2 = @"C:\qfolder\qfile.txt";
+//// Verbatim string literals
+//string path2 = @"C:\qfolder\qfile.txt";
 
-string multiLineMessage = """
-This is line 1
-    This is line 2
-        This is line 3
-""";
+//string multiLineMessage = """
+//This is line 1
+//    This is line 2
+//        This is line 3
+//""";
 
-Console.WriteLine(multiLineMessage);
+//Console.WriteLine(multiLineMessage);
 #endregion
 
 
+#region Value Types vs Reference Types
+int x = 10; // Value Type
+int y = x;  // y gets a copy of the value in x
+x = 20;     // changing x does not affect y
 
+Console.WriteLine($"x is {x}, y is {y}");
+// Output x is 20, y is 10
+
+
+string name1 = "Sameer Saini"; // name1 is a reference to a string object in the heap
+string name2 = name1; // name 2 now references the same string object
+
+name1 = "John Doe"; // // This creates a new string object, and str1 now references the new object
+
+Console.WriteLine($"Name1 is {name1}, Name2 is {name2}");
+//(name2 still references the original string)
+#endregion
