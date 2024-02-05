95a70d4625e40012dd6210d05039455d80abdc23
sameer8saini <sameer8saini@gmail.com>
2024-02-05 14:41:41 +1300

    Nullable Types

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿//string name = null;
+
+//int? age = null;
+
+//age = 27;
+
+//age = null;
+
+string firstName;
+string lastName;
+
+bool? hasSubscribed; // True, False, Null
+hasSubscribed = null;
+
+if (hasSubscribed.HasValue)
+{
+    Console.WriteLine("User has chosen a value");
+}
+else
+{
+    Console.WriteLine("User has chosen not to answer this question.");
+}
