be8150473755ce7afd6383f7ed0c9068fd0ce402
sameer8saini <sameer8saini@gmail.com>
2024-02-05 17:01:10 +1300

    Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,14 @@
+﻿//// 1
+//string[] colours = new string[3];
+//colours[0] = "Red";
+//colours[1] = "Green";
+//colours[2] = "Blue";
+
+//Console.WriteLine(colours[3]);
+
+//// 2
+//string[] colours = new[] { "Red", "Green", "Blue" };
+
+
+// 3
+string[] colours = ["Red", "Green", "Blue"];
