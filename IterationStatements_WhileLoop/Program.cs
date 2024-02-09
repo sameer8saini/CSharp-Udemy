68540da4c8f64df40a6f5a769ae4ad716f00315f
sameer8saini <sameer8saini@gmail.com>
2024-02-09 15:49:01 +1300

    While Loop

---
+++ IterationStatements_WhileLoop/Program.cs
@@ -0,0 +1,30 @@
+﻿#region While Loop
+//Console.WriteLine("Please input a number");
+
+//int userInput = int.Parse(Console.ReadLine());
+
+//int startingNumber = 0;
+
+//while (startingNumber <= userInput)
+//{
+//    Console.WriteLine(startingNumber);
+//    startingNumber = startingNumber + 2;
+//}
+
+//Console.WriteLine("End of program.");
+#endregion
+
+#region Infinite While Loop
+
+int startingNumber = 0;
+
+while (true)
+{
+    Console.WriteLine("Please enter a number");
+    int userInput = int.Parse(Console.ReadLine());
+    startingNumber += userInput;
+    Console.WriteLine("Total becomes " + startingNumber);
+}
+
+
+#endregion
