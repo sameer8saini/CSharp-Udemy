b872c24c1b5064a04d293d5e8cb67dceb0713f49
sameer8saini <sameer8saini@gmail.com>
2024-02-09 15:59:44 +1300

    Do While Loops

---
+++ IterationStatements_DoWhileLoop/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int startingNumber = 1;
+int product;
+Console.WriteLine("Please enter a number");
+int userInput = int.Parse(Console.ReadLine());
+
+do
+{
+    product = startingNumber* userInput;
+    Console.WriteLine($"{userInput} * {startingNumber} = {product}");
+    startingNumber += 1;
+}
+while (false);
+
