99c2fac18157f823f00648564259506fdfaec7c7
sameer8saini <sameer8saini@gmail.com>
2024-02-18 17:21:40 +1300

    Foreach loop

---
+++ IterationStatements_ForeachLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿//int[] numbers = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//int[] numbers = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int[] numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    Console.WriteLine(numbers[i]);
+//}
+
+
+foreach (int element in numbers)
+{
+    Console.WriteLine(element);
+}
+
+
+string[] words = ["This", "is", "a", "foreach", "loop"];
+
+foreach (string word in words)
+{
+    Console.Write(word);
+    Console.Write(" ");
+}
