01b607507cce1b32a195ba40a69d11d87b48de05
sameer8saini <sameer8saini@gmail.com>
2024-02-26 11:03:51 +1300

    String length

---
+++ Length/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+string userString = " rtybun";
+
+int numberOfCharacters = userString.Length;
+
+if (numberOfCharacters > 20)
+{
+    Console.WriteLine("Your message cannot be more than 20 characters");
+}
+
+Console.WriteLine("End of program");
