22fe404ad71d646d72f470e82b58c2ab1b0d01f1
sameer8saini <sameer8saini@gmail.com>
2024-02-26 11:49:07 +1300

    String Substring

---
+++ Substring/Program.cs
@@ -0,0 +1,10 @@
+﻿string originalString = "My name is John Doe!";
+
+
+
+string substringEx1 = originalString.Substring(3);
+
+string substringEx2 = originalString.Substring(3, 7);
+
+
+Console.WriteLine(substringEx2);
