040aa2560215170ca7c436308b397e25565f7544
sameer8saini <sameer8saini@gmail.com>
2024-02-26 13:50:29 +1300

    Lowercase and Uppercase Strings

---
+++ UpperAndLowerCase/Program.cs
@@ -0,0 +1,14 @@
+﻿// email@example.com
+// EmAiL@ExAmPle.COM
+
+
+
+string emailMixCase = "EmAiL@ExAmPle.COM";
+string emailLowercase = "email@example.com";
+
+if (emailMixCase.ToLower() == emailLowercase.ToLower())
+{
+    Console.WriteLine("Emails match");
+}
+
+Console.WriteLine("End of program");
