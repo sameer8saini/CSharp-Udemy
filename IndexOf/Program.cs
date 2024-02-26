70855c68139fe39ae900f441097f62031907586e
sameer8saini <sameer8saini@gmail.com>
2024-02-26 13:34:33 +1300

    IndexOf and LastIndexOf String Methods

---
+++ IndexOf/Program.cs
@@ -0,0 +1,15 @@
+﻿
+string originalString = "My name is John Doe!";
+
+// IndexOf
+
+//int index = originalString.IndexOf('N');
+//int index = originalString.IndexOf("Doe");
+
+
+// LastIndexOf
+
+int index = originalString.LastIndexOf('n');
+
+
+Console.WriteLine(index);
