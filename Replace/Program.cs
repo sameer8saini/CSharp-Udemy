97295a9cb593c2f9c397f53f97a2ebd848720388
sameer8saini <sameer8saini@gmail.com>
2024-02-26 14:13:33 +1300

    Replace method in String

---
+++ Replace/Program.cs
@@ -0,0 +1,20 @@
+﻿//string originalString = "Programming is fun with Java";
+
+
+//string newString = originalString.Replace("Java", "C#");
+
+//Console.WriteLine(originalString);
+//Console.WriteLine(newString);
+
+
+//string originalString = "Cat";
+
+//Console.WriteLine(originalString.Replace('c', 'B'));
+
+
+string paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit velit ac ex pharetra, sed fermentum nulla facilisis. In nec sollicitudin ante. John Doe Nam non magna id metus vestibulum euismod. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed vitae justo id velit feugiat hendrerit. John Doe Sed nec ante in libero fermentum vestibulum. Vivamus lobortis diam sit amet ex lobortis, ac interdum leo tempor. John Doe Quisque auctor, justo ac tristique malesuada, felis libero rhoncus dui, at sagittis justo tortor ut velit. Integer varius consequat elit John Doe ac egestas. Nullam nec sapien nec odio tincidunt tristique";
+
+
+string newParag = paragraph.Replace("John Doe", "Sameer Saini");
+
+Console.WriteLine(newParag);
