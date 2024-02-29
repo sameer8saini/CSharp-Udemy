0b249799ec3c76c3d4b00e3b5d67a7cf5136c438
sameer8saini <sameer8saini@gmail.com>
2024-03-01 09:41:49 +1300

    Split a string

---
+++ Split/Program.cs
@@ -0,0 +1,13 @@
+﻿string userInput = "apple,banana,orange,,pineapple";
+
+string tableHeader = "Id|Name|Phone|Email";
+
+
+//string[] subUserInput = userInput.Split(',');
+
+string[] subUserInput = userInput.Split(',', 100, StringSplitOptions.RemoveEmptyEntries);
+
+foreach (string item in subUserInput)
+{
+    Console.WriteLine(item);
+}
