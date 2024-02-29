19ab20c4f557385c8b214d13d00148b743601889
sameer8saini <sameer8saini@gmail.com>
2024-03-01 10:18:09 +1300

    Trim, TrimStart and TrimEnd

---
+++ Trim/Program.cs
@@ -0,0 +1,23 @@
+﻿
+string userInput = "   Some text here    ";
+Console.WriteLine(userInput.Trim());
+// "Some text here"
+
+
+string userInput1 = "$$$$$   Some text here    $$$$$";
+Console.WriteLine(userInput1.Trim('$'));
+// "   Some text here    "
+
+
+string userInput2 = "$$$$$   Some text here    $$$$$";
+Console.WriteLine(userInput2.Trim('$', ' '));
+// "Some text here"
+
+
+string userInput3 = "$$$$$   Some text here    $$$$$";
+Console.WriteLine(userInput3.TrimStart('$'));
+// "   Some text here    $$$$$"
+
+string userInput4 = "$$$$$   Some text here    $$$$$";
+Console.WriteLine(userInput4.TrimEnd('$'));
+// "$$$$$   Some text here    "
