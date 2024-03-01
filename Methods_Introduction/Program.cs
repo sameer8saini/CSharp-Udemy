50b30cf3893545fb4786273c95ecf2d596481806
sameer8saini <sameer8saini@gmail.com>
2024-03-01 16:34:50 +1300

    Methods Introduction, Instance Methods and Static Methods

---
+++ Methods_Introduction/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Methods_Introduction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call Instance Method
+            Program programRef = new Program();
+            programRef.PrintCurrentYear();
+
+            // Call Static Method
+            Program.PrintDate();
+        }
+
+        public void PrintCurrentYear()
+        {
+            DateTime currentDateTime = DateTime.Now;
+
+            int currentYear = currentDateTime.Year;
+
+            Console.WriteLine(currentYear);
+        }
+
+        public static void PrintDate()
+        {
+            DateTime dateToday = DateTime.Now;
+            Console.WriteLine(dateToday);
+        }
+    }
+}
