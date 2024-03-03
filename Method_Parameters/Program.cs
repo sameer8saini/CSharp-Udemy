6b928e6b19da3ffe2eca0b520882cbb2ee065307
sameer8saini <sameer8saini@gmail.com>
2024-03-04 12:50:11 +1300

    Method Parameters Introduction

---
+++ Method_Parameters/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Method_Parameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program programObj = new Program();
+            double lengthOfRec1 = 10;
+            double width = 5;
+            double result = programObj.CalculateAreaOfRectangle(lengthOfRec1, width);
+
+            Console.WriteLine($"Area of rectangle with length of {lengthOfRec1} and " +
+                $"width of {width} is {result}");
+        }
+
+
+        public double CalculateAreaOfRectangle(double length, double width)
+        {
+            double area = length * width;
+            return area;
+            //  return length * width
+        }
+
+
+    }
+}
