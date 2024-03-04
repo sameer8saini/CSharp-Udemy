635525f2cafd9574a2be6bd5558ad36270ed9095
sameer8saini <sameer8saini@gmail.com>
2024-03-04 14:36:33 +1300

    Value parameters

---
+++ Value_Parameters/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Value_Parameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program programObj = new Program();
+
+            int i = 10;
+            programObj.ValueParametersExplained(i);
+            Console.WriteLine($"Value of i is {i}");
+        }
+
+
+        public void ValueParametersExplained(int j)
+        {
+            j = 27;
+            Console.WriteLine($"Value of j is {j}");
+        }
+
+
+    }
+}
