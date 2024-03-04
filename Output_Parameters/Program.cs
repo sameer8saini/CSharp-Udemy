bae5b03c6dca2df5b37cb5346ecfe67c7ef40a80
sameer8saini <sameer8saini@gmail.com>
2024-03-04 15:00:28 +1300

    Output Types

---
+++ Output_Parameters/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Output_Parameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program programObj = new Program();
+            programObj.GetSquareAndCube(5, out int squareResult, out int cubeResult);
+
+            Console.WriteLine($"Square of 5 is {squareResult} and cube is {cubeResult}");
+        }
+
+
+        public void GetSquareAndCube(int number, out int square, out int cube)
+        {
+            square = number * number;
+            cube = number * number * number;
+        }
+
+    }
+}
