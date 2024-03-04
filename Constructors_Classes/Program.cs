21f349f7698476a794e1db58c0ac92493f9bee94
sameer8saini <sameer8saini@gmail.com>
2024-03-05 11:13:26 +1300

    Constructors in C#

---
+++ Constructors_Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿
+// This is for Default constructors
+//Car obj1 = new Car();
+
+Car obj1 = new Car("Honda", "Jazz");
+
+obj1.PrintCarName();
+
+
+public class Car
+{
+    public string Make;
+    public string Model;
+
+    // Default Constructor
+    //public Car()
+    //{
+    //    Make = "Tesla";
+    //    Model = "Model Y";
+    //}
+
+
+    public Car(string makeValue, string modelValue)
+    {
+        Make = makeValue;
+        Model = modelValue;
+    }
+
+
+    public void PrintCarName()
+    {
+        Console.WriteLine($"The car is a {Make} {Model}");
+    }
+}
