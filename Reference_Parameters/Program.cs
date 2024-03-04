82b0e6c57a723cd4b9e4bd9b5802fad7b7cf0b80
sameer8saini <sameer8saini@gmail.com>
2024-03-04 14:45:59 +1300

    Reference Types (Pass by ref)

---
+++ Reference_Parameters/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Reference_Parameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program programObj = new Program();
+
+            int i = 10;
+            programObj.ReferenceParametersExplained(ref i);
+            Console.WriteLine($"Value of i is {i}");
+        }
+
+        public void ReferenceParametersExplained(ref int j)
+        {
+            j = 27;
+            Console.WriteLine($"Value of j is {j}");
+        }
+    }
+}
