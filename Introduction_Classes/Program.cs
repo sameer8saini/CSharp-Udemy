c98ffb938ff121789c10a99f9105427088853ad1
sameer8saini <sameer8saini@gmail.com>
2024-03-04 17:39:27 +1300

    Introduction to classes

---
+++ Introduction_Classes/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+Employee employeeOne = new Employee();
+employeeOne.FirstName = "John";
+employeeOne.LastName = "Doe";
+employeeOne.Salary = 20000;
+employeeOne.YearsInService = 5;
+employeeOne.PrintFullName();
+
+
+Employee employeeTwo = new Employee()
+{
+    FirstName = "Sameer",
+    LastName = "Saini",
+    Salary = 30000,
+    YearsInService = 0
+};
+employeeTwo.PrintFullName();
+
+
+public class Employee()
+{
+    public string FirstName;
+    public string LastName;
+    public int Salary;
+    public int YearsInService;
+
+
+    public void PrintFullName()
+    {
+        Console.WriteLine($"The full name is {FirstName} {LastName}");
+    }
+}
