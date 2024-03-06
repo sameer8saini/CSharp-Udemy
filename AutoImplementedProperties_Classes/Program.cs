d431bbf3699c1692b9ce4595a413a4a8be433d75
sameer8saini <sameer8saini@gmail.com>
2024-03-06 16:44:53 +1300

    Auto-Implemented Properties

---
+++ AutoImplementedProperties_Classes/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Customer c1 = new Customer();
+c1.CustomerId = 1;
+c1.Address = "123 Some Street";
+
+
+
+public class Customer
+{
+    public int CustomerId { get; set; }
+    public string CustomerName { get; set; } = "Sameer Saini";
+    public string Address { get; set; }
+
+
+    //public Customer(int customerId, string customerName, string address)
+    //{
+    //    CustomerId = customerId;
+    //    CustomerName = customerName;
+    //    Address = address;
+    //}
+
+    // Method
+}
