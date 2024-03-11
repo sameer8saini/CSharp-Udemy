92e9c744eddeadcfe7704fddb22dce98d7383d7a
sameer8saini <sameer8saini@gmail.com>
2024-03-11 16:50:52 +1300

    Abstract classes vs Interfaces

--- Interfaces_vs_AbstractClasses/Program.cs
+++ Interfaces_vs_AbstractClasses/Program.cs
@@ -1,1 +1,47 @@
 ﻿
+
+
+public abstract class Shape
+{
+    public int Number;
+
+    public abstract void Method();
+}
+
+public interface IShape
+{
+    // Allowed
+    private static int Number;
+
+    private void Method()
+    {
+        Console.WriteLine("This is a method");
+    }
+
+
+    // Not Allowed
+    //public int Number;
+}
+
+
+public class C1 : I1, I2, IShape
+{
+
+}
+
+public abstract class C2
+{
+
+}
+
+
+
+public interface I1
+{
+
+}
+
+public interface I2
+{
+
+}
