941247bfeeda478063605b45cd70cd4b1d93f0a0
sameer8saini <sameer8saini@gmail.com>
2024-03-11 15:14:54 +1300

    Abstract Classes

---
+++ AbstractClass/Program.cs
@@ -0,0 +1,34 @@
+﻿Circle circleOne = new Circle(5);
+double result =  circleOne.CalculateArea();
+Console.WriteLine($"The area of circle with radius 5 is {result}");
+circleOne.Print();
+
+
+public abstract class Shape
+{
+    // Abstract Method
+    public abstract double CalculateArea();
+
+    // Concrete Method
+    public void Print()
+    {
+        Console.WriteLine("This is the implementation of the Print method");
+    }
+}
+
+
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
---
+++ Interface_Introduction/Program.cs
@@ -0,0 +1,58 @@
+﻿Rectangle rect1 = new Rectangle();
+rect1.Length = 5;
+rect1.Width = 10;
+double perimeterOfRect1 = rect1.CalculatePerimeter();
+double areaOfRect1 = rect1.CalculateArea();
+Console.WriteLine($"The area and perimeter of a rectangle is {areaOfRect1}, {perimeterOfRect1}");
+
+
+Triangle t1 = new Triangle();
+t1.SideOne = 9;
+t1.SideTwo = 11;
+t1.SideThree = 16;
+double perimeterOft1 = t1.CalculatePerimeter();
+double areaOft1 = t1.CalculateArea();
+Console.WriteLine($"The area and perimeter of a triangle is {areaOft1}, {perimeterOft1}");
+
+
+
+public class Rectangle : IPolygon
+{
+    public double Length { get; set; }
+    public double Width { get; set; }
+
+    public double CalculateArea()
+    {
+        return Length * Width;
+    }
+
+    public double CalculatePerimeter()
+    {
+        return 2 * (Length + Width);
+    }
+}
+
+public class Triangle : IPolygon
+{
+    public double SideOne { get; set; }
+    public double SideTwo { get; set; }
+    public double SideThree { get; set; }
+
+    public double CalculateArea()
+    {
+        double s = (SideOne + SideTwo + SideThree) / 2;
+        return Math.Sqrt(s * (s - SideOne) * (s - SideTwo) * (s - SideThree));
+    }
+
+    public double CalculatePerimeter()
+    {
+        return SideOne + SideTwo + SideThree;
+    }
+}
+
+
+public interface IPolygon
+{
+    double CalculateArea();
+    double CalculatePerimeter();
+}
