a1829e781ba15de9b75c0a662fc680b8d1a7abcc
sameer8saini <sameer8saini@gmail.com>
2024-03-11 16:37:53 +1300

    Default Interface Methods

---
+++ Explicit_Implementation_Interfaces/Program.cs
@@ -0,0 +1,24 @@
+﻿
+IPolygon I1 = new Rectangle();
+I1.Print();
+
+
+public class Rectangle : IPolygon
+{
+    public double CalculateArea()
+    {
+        Console.WriteLine("This is implementation for CalculateArea");
+        return 0d;
+    }
+}
+
+
+public interface IPolygon
+{
+    public void Print()
+    {
+        Console.WriteLine("This is the Implementation from IPolygon interface");
+    }
+
+    double CalculateArea();
+}
