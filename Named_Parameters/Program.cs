5bd4d9655455ff8abea1c5ea123d5153b8d7b390
sameer8saini <sameer8saini@gmail.com>
2024-05-14 14:56:42 +1200

    Named Parameters

---
+++ Named_Parameters/Program.cs
@@ -0,0 +1,24 @@
+﻿void PrintOrderDetails(string sellerName, int orderId, 
+    string productName, int quantity = 1, int quantityLeft = 1)
+{
+    Console.WriteLine("Seller: " + sellerName);
+    Console.WriteLine("Order ID: " + orderId);
+    Console.WriteLine("Product: " + productName);
+    Console.WriteLine("Quantity: " + quantity);
+    Console.WriteLine("Quantity Left: " + quantityLeft);
+}
+
+
+
+
+PrintOrderDetails("Apple", 1, "Orange", 1, 1);
+
+
+PrintOrderDetails(sellerName: "Apple", orderId: 1, productName: "Apple",
+    quantity: 2, quantityLeft: 5);
+
+
+PrintOrderDetails(sellerName: "Apple", orderId: 1, productName: "Apple", 
+    quantityLeft: 10);
+
+PrintOrderDetails(orderId: 1, productName: "Apple", sellerName: "Apple");
