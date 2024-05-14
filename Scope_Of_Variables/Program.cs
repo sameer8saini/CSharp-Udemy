c06fa139405b738c32616c27e200a633c3894bc9
sameer8saini <sameer8saini@gmail.com>
2024-05-14 13:29:57 +1200

    Scope Of Variables

---
+++ Scope_Of_Variables/Program.cs
@@ -0,0 +1,27 @@
+﻿string name = "Sameer";
+
+void MethodOne()
+{
+    name = "Sameer Saini";
+    string anotherVariable = "Some value"; 
+}
+
+void MethodTwo()
+{
+    name = "John Doe";
+    //Console.WriteLine(anotherVariable);
+    //Console.WriteLine(i);
+    //Console.WriteLine(j);
+
+    for (int i = 0; i < 10; i++)
+    {
+        var j = i + 1;
+        Console.WriteLine(i);
+        Console.WriteLine(j);
+    }
+}
+
+
+MethodOne();
+Console.WriteLine(name);
+//Console.WriteLine(anotherVariable);
